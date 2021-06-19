ccd70c4fe87af086416f837d980f14faea3419ef
Mukammed Alimbet <alimbetov.mu@gmail.com>
2021-06-19 01:04:10 -0700

    a

--- bert_api/bert/predict.py
+++ bert_api/bert/predict.py
@@ -1,4 +1,6 @@
 from deeppavlov import build_model
+from deeppavlov.utils.server import start_model_server
+
 import json
 with open('bert_api/data/model_config.json') as f:
   data = json.load(f)
@@ -6,10 +8,9 @@ with open('bert_api/data/model_config.json') as f:
 
 
 def main():
-    model = build_model(data)
-    return model
+    start_model_server(data)
 
 
 #COMMANDS
-#python -m deeppavlov riseapi bert_api/data/model_config.json -b 2
+#python -m deeppavlov riseapi bert_api/data/model_config.json -b 2 or #poetry run script
 #curl -X POST "http://0.0.0.0:5000/model" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"x\":[\" \"]}"
