37ab744d4e1230338e04c3e55a90a81cddf739e9
Mukammed Alimbet <alimbetov.mu@gmail.com>
2021-06-19 01:06:26 -0700

    deleting log

--- bert_api/bert/predict.py
+++ bert_api/bert/predict.py
@@ -9,8 +9,3 @@ with open('bert_api/data/model_config.json') as f:
 
 def main():
     start_model_server(data)
-
-
-#COMMANDS
-#python -m deeppavlov riseapi bert_api/data/model_config.json -b 2 or #poetry run script
-#curl -X POST "http://0.0.0.0:5000/model" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"x\":[\" \"]}"
